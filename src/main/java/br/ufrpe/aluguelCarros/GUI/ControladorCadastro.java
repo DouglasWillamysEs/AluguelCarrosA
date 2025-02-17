9d12973dd5561910a354b7c395bb4237910ce313
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2025-02-17 00:25:20 -0300

    ControladorCadastro.java

--- src/main/java/br/ufrpe/aluguelCarros/GUI/ControladorCadastro.java
+++ src/main/java/br/ufrpe/aluguelCarros/GUI/ControladorCadastro.java
@@ -56,7 +56,7 @@ public class ControladorCadastro {
     }
 
     @FXML
-    private void radioUsuarioSelecionado() {a
+    private void radioUsuarioSelecionado() {
         txtChave.setVisible(false);
     }
 
