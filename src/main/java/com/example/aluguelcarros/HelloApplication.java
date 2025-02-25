5661bb4d5733e6f6ab03ab9fde5e141f96713fae
Miguel Cavalcanti Rufino <miguel.cavalcanti@ufrpe.br>
2025-02-24 23:35:51 -0300

    app

--- src/main/java/com/example/aluguelcarros/HelloApplication.java
+++ src/main/java/com/example/aluguelcarros/HelloApplication.java
@@ -12,7 +12,7 @@
         public void start(Stage stage) throws IOException {
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-            stage.setTitle("Hello!");
+            stage.setTitle("Hello, GUYS!");
             stage.setScene(scene);
             stage.show();
         }
