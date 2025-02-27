25e8b47c7443798bc6d93bac022e15733e6ef877
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2025-02-27 02:35:11 -0300

    ControladorBusca.java

--- src/main/java/br/ufrpe/aluguelCarros/GUI/ControladorBusca.java
+++ src/main/java/br/ufrpe/aluguelCarros/GUI/ControladorBusca.java
@@ -1,41 +1,73 @@
 package br.ufrpe.aluguelcarros.GUI;
 
 import br.ufrpe.aluguelcarros.negocio.ControladorCarro;
+import br.ufrpe.aluguelcarros.negocio.beans.Aluguel;
 import br.ufrpe.aluguelcarros.negocio.beans.Carro;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.input.DragEvent;
-
-
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 import java.util.ArrayList;
 
 public class ControladorBusca {
 
-    private ControladorCarro controladorCarro = ControladorCarro.getInstance();
 
     @FXML
-    private ChoiceBox<String> cbHatchbacks; // Define o tipo de dados correto
+    private ChoiceBox<String> cbMarca;
+
+    @FXML
+    private ChoiceBox<String> cbModelo;
+
+    @FXML
+    private CheckBox checkMaiorPreco;
+
+    @FXML
+    private CheckBox checkMenorPreco;
+
+    @FXML
+    private ChoiceBox<String> txtCor;
+
+    @FXML
+    private ListView<String> lvListView;
+
+    @FXML
+    void cbMarca(MouseEvent event) {
+
+    }
+
+    @FXML
+    void maiorPrecoSelecionado(ActionEvent event) {
+        checkMenorPreco.setSelected(false);
+    }
+
+    @FXML
+    void menorPrecoSelecionado(ActionEvent event) {
+        checkMaiorPreco.setSelected(false);
+    }
+
+    @FXML
+    private ListView<String> listViewCarros;
 
     @FXML
     public void initialize() {
-        carregarHatchbacks();
-    }
 
-    private void carregarHatchbacks() {
-        ArrayList<String> hatchbacks = new ArrayList<>();
-            ControladorCarro.getInstance().procurarModelo("hatchback");
+        ControladorCarro.getInstance().CadastrarCarroC("Azuil", "Chevrolet", "Prisma", 38, true);
+        ControladorCarro.getInstance().CadastrarCarroC("Amerilo", "bom dia", "Prisma", 38, true);
 
-        ObservableList<String> opcoes = FXCollections.observableArrayList(hatchbacks);
-        cbHatchbacks.setItems(opcoes);
-    }
+        // Adicionando itens nas ChoiceBoxes
+        cbMarca.getItems().addAll(ControladorCarro.getInstance().retornarMarcasCarros());
+        cbModelo.getItems().addAll(ControladorCarro.getInstance().retornarModeloCarros());
+        txtCor.getItems().addAll(ControladorCarro.getInstance().retornarCorCarros());
 
-    @FXML
-    void hatchbacksSelecionado(DragEvent event) {
-
+        // Definindo um valor padrão (opcional)
+        cbMarca.setValue("Marca");
+        cbModelo.setValue("Modelo");
+        txtCor.setValue("Cor");
     }
 
 
 }
+
