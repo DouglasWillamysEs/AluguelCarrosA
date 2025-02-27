3caa7dd56117d209595a6e671e7e89a70076f3e9
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2025-02-27 10:53:36 -0300

    UpdateControladorBusca.java

--- src/main/java/br/ufrpe/aluguelCarros/GUI/ControladorBusca.java
+++ src/main/java/br/ufrpe/aluguelCarros/GUI/ControladorBusca.java
@@ -1,19 +1,26 @@
 package br.ufrpe.aluguelcarros.GUI;
 
 import br.ufrpe.aluguelcarros.negocio.ControladorCarro;
-import br.ufrpe.aluguelcarros.negocio.beans.Aluguel;
 import br.ufrpe.aluguelcarros.negocio.beans.Carro;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class ControladorBusca {
-
+public class ControladorBusca implements Initializable {
 
     @FXML
     private ChoiceBox<String> cbMarca;
@@ -22,52 +29,114 @@ public class ControladorBusca {
     private ChoiceBox<String> cbModelo;
 
     @FXML
-    private CheckBox checkMaiorPreco;
-
-    @FXML
-    private CheckBox checkMenorPreco;
-
-    @FXML
     private ChoiceBox<String> txtCor;
 
     @FXML
-    private ListView<String> lvListView;
+    private ListView<Carro> lvListView;
 
     @FXML
     void cbMarca(MouseEvent event) {
-
     }
 
     @FXML
-    void maiorPrecoSelecionado(ActionEvent event) {
-        checkMenorPreco.setSelected(false);
+    private void onMouse(MouseEvent event) {
     }
 
-    @FXML
-    void menorPrecoSelecionado(ActionEvent event) {
-        checkMaiorPreco.setSelected(false);
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        ControladorCarro.getInstance().CadastrarCarroC("Azul", "Chevrolet", "Prisma", 120, true);
+        ControladorCarro.getInstance().CadastrarCarroC("Amarelo", "Chevrolet", "Onix", 80, true);
+        ControladorCarro.getInstance().CadastrarCarroC("Vermelho", "Fiat", "Uno", 60, true);
+
+        cbMarca.getItems().clear();
+        cbModelo.getItems().clear();
+        txtCor.getItems().clear();
+        lvListView.getItems().clear();
+
+        ArrayList<String> marcas = ControladorCarro.getInstance().retornarMarcasCarros();
+        ArrayList<String> modelos = ControladorCarro.getInstance().retornarModeloCarros();
+        ArrayList<String> cores = ControladorCarro.getInstance().retornarCorCarros();
+
+        cbMarca.getItems().add("Todas");
+        cbMarca.getItems().addAll(marcas);
+        cbModelo.getItems().add("Todos");
+        cbModelo.getItems().addAll(modelos);
+        txtCor.getItems().add("Todas");
+        txtCor.getItems().addAll(cores);
+
+        cbMarca.setValue("Todas");
+        cbModelo.setValue("Todos");
+        txtCor.setValue("Todas");
+
+        atualizarListView();
+
+        cbMarca.setOnAction(event -> atualizarListView());
+        cbModelo.setOnAction(event -> atualizarListView());
+        txtCor.setOnAction(event -> atualizarListView());
+
+        lvListView.setOnMouseClicked(event -> {
+            Carro itemSelecionado = lvListView.getSelectionModel().getSelectedItem();
+            if (itemSelecionado != null) {
+                tratarItemSelecionado(itemSelecionado);
+            }
+        });
+
+        lvListView.setCellFactory(new Callback<>() {
+            @Override
+            public javafx.scene.control.ListCell<Carro> call(javafx.scene.control.ListView<Carro> param) {
+                return new javafx.scene.control.ListCell<>() {
+                    @Override
+                    protected void updateItem(Carro item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item != null) {
+                            setText(item.getMarca() + " " + item.getModelo() + " (" + item.getCor() + ")");
+                        } else {
+                            setText(null);
+                        }
+                    }
+                };
+            }
+        });
     }
 
-    @FXML
-    private ListView<String> listViewCarros;
+    private void atualizarListView() {
+        String marcaSelecionada = cbMarca.getValue();
+        String modeloSelecionado = cbModelo.getValue();
+        String corSelecionada = txtCor.getValue();
 
-    @FXML
-    public void initialize() {
+        ArrayList<Carro> todosCarros = ControladorCarro.getInstance().retornarTodosCarros(); // Obtém todos os carros
+        ArrayList<Carro> carrosFiltrados = new ArrayList<>();
 
-        ControladorCarro.getInstance().CadastrarCarroC("Azuil", "Chevrolet", "Prisma", 38, true);
-        ControladorCarro.getInstance().CadastrarCarroC("Amerilo", "bom dia", "Prisma", 38, true);
+        for (Carro carro : todosCarros) {
+            boolean marcaOk = marcaSelecionada.equals("Todas") || carro.getMarca().equals(marcaSelecionada);
+            boolean modeloOk = modeloSelecionado.equals("Todos") || carro.getModelo().equals(modeloSelecionado);
+            boolean corOk = corSelecionada.equals("Todas") || carro.getCor().equals(corSelecionada);
 
-        // Adicionando itens nas ChoiceBoxes
-        cbMarca.getItems().addAll(ControladorCarro.getInstance().retornarMarcasCarros());
-        cbModelo.getItems().addAll(ControladorCarro.getInstance().retornarModeloCarros());
-        txtCor.getItems().addAll(ControladorCarro.getInstance().retornarCorCarros());
+            if (marcaOk && modeloOk && corOk) {
+                carrosFiltrados.add(carro);
+            }
+        }
 
-        // Definindo um valor padrão (opcional)
-        cbMarca.setValue("Marca");
-        cbModelo.setValue("Modelo");
-        txtCor.setValue("Cor");
+        lvListView.getItems().setAll(carrosFiltrados);
+    }
+
+    private void tratarItemSelecionado(Carro carroSelecionado) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/ufrpe/aluguelcarros/AluguelAgora.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Aluguel Agora");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
 }
+
 
