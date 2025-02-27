f3dc6db04f96fea6214e4da3d382df3409c95ba4
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2025-02-27 02:39:07 -0300

    Update IRepositorioCarros.java

--- src/main/java/br/ufrpe/aluguelCarros/dados/IRepositorioCarros.java
+++ src/main/java/br/ufrpe/aluguelCarros/dados/IRepositorioCarros.java
@@ -6,9 +6,13 @@ import java.util.ArrayList;
 
 public interface IRepositorioCarros {
 
-    void cadastrarCarro(int id, String cor, String marca, String modelo, double preco, boolean statusPagamento);
+    void cadastrarCarro(String cor, String marca, String modelo, double preco, boolean statusPagamento);
     void removerCarro(int id);
     void editarCarro(int id, String cor, String marca, String modelo, double preco);
     Carro procurarCarro(int id);
     ArrayList<Carro> procurarModelo(String modelo);
+    ArrayList<String> retornarMarcas();
+    ArrayList<String> retornarCores();
+    ArrayList<String> retornarModelos();
+
 }
